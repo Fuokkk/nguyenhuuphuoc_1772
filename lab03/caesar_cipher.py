542ea3982236aee34554bb8498b32c588eaf4fee
Fuokk <mcvnphuoc@gmail.com>
2026-03-18 09:57:19 +0700

    [lab03]

--- lab03/caesar_cipher.py
+++ lab03/caesar_cipher.py
@@ -9,22 +9,22 @@ class MyApp(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-        # Sửa tên từ btn_encrypt -> pushButton
+        
         self.ui.pushButton.clicked.connect(self.call_api_encrypt)
-        # Sửa tên từ btn_decrypt -> pushButton_2
+        
         self.ui.pushButton_2.clicked.connect(self.call_api_decrypt)
 
     def call_api_encrypt(self):
         url = "http://127.0.0.1:5000/api/caesar/encrypt"
         payload = {
-            "plain_text": self.ui.textEdit.toPlainText(), # textEdit là Plain Text
-            "key": self.ui.textEdit_2.toPlainText()      # textEdit_2 là Key
+            "plain_text": self.ui.textEdit.toPlainText(), 
+            "key": self.ui.textEdit_2.toPlainText()      
         }
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                # Sửa tên từ txt_cipher_text -> textEdit_3
+               
                 self.ui.textEdit_3.setText(data["encrypted_message"])
                 
                 msg = QMessageBox()
@@ -39,14 +39,14 @@ class MyApp(QMainWindow):
     def call_api_decrypt(self):
         url = "http://127.0.0.1:5000/api/caesar/decrypt"
         payload = {
-            "cipher_text": self.ui.textEdit_3.toPlainText(), # textEdit_3 là Cipher Text
+            "cipher_text": self.ui.textEdit_3.toPlainText(), 
             "key": self.ui.textEdit_2.toPlainText()
         }
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                # Trả kết quả về lại textEdit (Plain Text)
+                
                 self.ui.textEdit.setText(data["decrypted_message"])
                 
                 msg = QMessageBox()
