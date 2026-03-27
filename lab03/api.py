94270fcd3684ff8e6bd1b0f9f5abf0c7262a1c40
Fuok <mcvnphuoc@gmail.com>
2026-03-27 21:13:00 +0700

    [add]

--- lab03/api.py
+++ lab03/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import os
-# Đảm bảo đường dẫn import chính xác với cấu trúc lab03/cipher/rsa/rsa_cipher.py
+from cipher.ecc import ECCCipher
 from cipher.rsa.rsa_cipher import RSACipher
 
 app = Flask(__name__)
@@ -85,6 +85,30 @@ def rsa_verify_signature():
         return jsonify({'is_verified': is_verified})
     except Exception as e:
         return jsonify({'is_verified': False, 'error': str(e)})
+ecc_cipher = ECCCipher()
 
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    data = request.json
+    message = data['message']
+    private_key, _ = ecc_cipher.load_keys()
+    signature = ecc_cipher.sign(message, private_key)
+    signature_hex = signature.hex()
+    return jsonify({'signature': signature_hex})
+
+@app.route('/api/ecc/verify', methods=['POST'])
+def ecc_verify_signature():
+    data = request.json
+    message = data['message']
+    signature_hex = data['signature']
+    _, public_key = ecc_cipher.load_keys()
+    signature = bytes.fromhex(signature_hex)
+    is_verified = ecc_cipher.verify(message, signature, public_key)
+    return jsonify({'is_verified': is_verified})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
